513d7e5aaa874adb366340142b2180fec7c520bf
Scott Lundberg <scottmlundberg@gmail.com>
2015-08-07 15:31:51 -0700

    First code commit.

--- src/StreamingTimeSeries.jl
+++ src/StreamingTimeSeries.jl
@@ -1,5 +1,36 @@
 module StreamingTimeSeries
 
-# package code goes here
+using Dates
+
+export EMAFeature, update!, value
+
+type EMAFeature
+    decayRate::Float64
+    lastTime::DateTime
+    lastValue::Float64
+    value::Float64
+end
+
+# see EMA_lin in http://www.eckner.com/papers/ts_alg.pdf
+function update!(feature::EMAFeature, time::DateTime, value::Float64)
+    @assert time >= feature.lastTime
+    if time == feature.lastTime
+        return feature.value
+    end
+
+    dt = int(time - feature.lastTime)/60000 # convert from milliseconds to minutes
+    a = dt / feature.decayRate
+    u = exp(-a)
+    v = (1 - u) / a
+    feature.value = u*feature.value + (v - u)*feature.lastValue + (1.0 - v)*value
+    feature.lastTime = time
+    feature.lastValue = value
+
+    feature.value
+end
+
+function value(feature::EMAFeature)
+    feature.value
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using StreamingTimeSeries
 using Base.Test
+using Dates
 
 # write your own tests here
-@test 1 == 1
+f = EMAFeature(1.0, DateTime(), 0.0, 0.0)
+@test value(f) == 0.0
+update!(f, DateTime(4050,12,1), 1.0)
+@test abs(value(f) - 1.0) < 0.00001
