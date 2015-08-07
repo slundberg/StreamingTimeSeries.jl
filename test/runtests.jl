50c6b607a86e400a412cffb0b104293747a6d0a8
Scott Lundberg <scottmlundberg@gmail.com>
2015-08-07 15:33:27 -0700

    Added test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,7 @@ using Dates
 # write your own tests here
 f = EMAFeature(1.0, DateTime(), 0.0, 0.0)
 @test value(f) == 0.0
-update!(f, DateTime(4050,12,1), 1.0)
+update!(f, DateTime(4050,12,1,1,1,1), 1.0)
+@test abs(value(f) - 1.0) < 0.00001
+update!(f, DateTime(4050,12,1,1,1,1), 1.0)
 @test abs(value(f) - 1.0) < 0.00001
