1c92528f29016ecdbdd0b01e8309afa387f93b23
Scott Lundberg <scottmlundberg@gmail.com>
2015-08-15 09:32:20 -0700

    Added EMVFeature

--- src/StreamingTimeSeries.jl
+++ src/StreamingTimeSeries.jl
@@ -1,6 +1,6 @@
 module StreamingTimeSeries
 
-export EMAFeature, TimeSinceFeature, LastValueFeature, update!, valueat
+export EMAFeature, EMVFeature, TimeSinceFeature, LastValueFeature, update!, valueat
 
 # see EMA_lin in http://www.eckner.com/papers/ts_alg.pdf
 type EMAFeature
@@ -9,13 +9,14 @@ type EMAFeature
     lastValue::Float64
     value::Float64
 end
+EMAFeature(decayRate::Float64) = EMAFeature(decayRate, DateTime(), 0.0, 0.0)
 function update!(feature::EMAFeature, time::DateTime, value::Float64)
     @assert time >= feature.lastTime
     if time == feature.lastTime
         return feature.value
     end
 
-    dt = int(time - feature.lastTime)/60000 # convert from milliseconds to minutes
+    dt = Float64(time - feature.lastTime)/60000 # convert from milliseconds to minutes
     a = dt / feature.decayRate
     u = exp(-a)
     v = (1 - u) / a
@@ -29,6 +30,27 @@ function valueat(feature::EMAFeature, time::DateTime)
     feature.value
 end
 
+type EMVFeature
+    ema::EMAFeature
+    ema2::EMAFeature
+    value::Float64
+end
+EMVFeature(decayRate::Float64) = EMVFeature(EMAFeature(decayRate), EMAFeature(decayRate), 0.0)
+function update!(feature::EMVFeature, time::DateTime, value::Float64)
+    @assert time >= feature.ema.lastTime
+    if time == feature.ema.lastTime
+        return feature.value
+    end
+
+    update!(feature.ema, time, value)
+    update!(feature.ema2, time, value^2)
+
+    feature.value = feature.ema2.value - feature.ema.value^2
+end
+function valueat(feature::EMVFeature, time::DateTime)
+    feature.value
+end
+
 # simply keep track of how far we are from the reference time point
 # note that we collapse all negative values to 0.0 since otherwise they
 # would provide information about the future.
@@ -39,7 +61,7 @@ function update!(feature::TimeSinceFeature, time::DateTime)
     feature.referenceTime = time
 end
 function valueat(feature::TimeSinceFeature, time::DateTime)
-    numMin = int(time - feature.referenceTime)/60000 # convert from milliseconds to minutes
+    numMin = Float64(time - feature.referenceTime)/60000 # convert from milliseconds to minutes
     numMin < 0.0 ? 0.0 : numMin
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,29 @@ using StreamingTimeSeries
 using Base.Test
 
 # EMAFeature
-f = EMAFeature(1.0, DateTime(), 0.0, 0.0)
+f = EMAFeature(1.0)
 @test valueat(f, DateTime(2015,8,7)) == 0.0
 update!(f, DateTime(4050,12,1,1,1,1), 1.0)
 @test abs(valueat(f, DateTime(2015,8,7)) - 1.0) < 0.00001
 update!(f, DateTime(4050,12,1,1,1,1), 1.0)
 @test abs(valueat(f, DateTime(2015,8,7)) - 1.0) < 0.00001
+
+# EMVFeature
+f = EMVFeature(2.0)
+@test valueat(f, DateTime(2015,8,7)) == 0.0
+update!(f, DateTime(2015,8,15,9,0,0), 1.0)
+update!(f, DateTime(2015,8,15,10,0,0), 10.0)
+update!(f, DateTime(2015,8,15,11,0,0), 1.0)
+update!(f, DateTime(2015,8,15,12,0,0), 10.0)
+@test valueat(f, DateTime(2015,8,15,12,0,0)) > 0.1
+update!(f, DateTime(2015,8,15,12,0,0), 2.0)
+update!(f, DateTime(2015,8,15,13,0,0), 2.0)
+update!(f, DateTime(2015,8,15,14,0,0), 2.0)
+update!(f, DateTime(2015,8,15,15,0,0), 2.0)
+update!(f, DateTime(2015,8,15,16,0,0), 2.0)
+update!(f, DateTime(2015,8,15,17,0,0), 2.0)
+update!(f, DateTime(2015,8,15,18,0,0), 2.0)
+@test valueat(f, DateTime(2015,8,15,12,0,0)) < 0.1
 
 # TimeSinceFeature
 f = TimeSinceFeature(DateTime(2015,8,7,1,1,1))
