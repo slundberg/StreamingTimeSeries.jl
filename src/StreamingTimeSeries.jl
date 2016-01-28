b4fa699b9289fbcae621b989283e802a61308c29
Scott Lundberg <scottmlundberg@gmail.com>
2016-01-28 14:44:49 -0800

    Tempting to support DateTime or ZonedDateTime

--- src/StreamingTimeSeries.jl
+++ src/StreamingTimeSeries.jl
@@ -1,16 +1,18 @@
 module StreamingTimeSeries
 
+import Base.Dates.TimeType
+
 export EMAFeature, EMVFeature, TimeSinceFeature, LastValueFeature, DecayFeature, LagSumFeature, update!, valueat
 
 # see EMA_lin in http://www.eckner.com/papers/ts_alg.pdf
-type EMAFeature
+type EMAFeature{T<:TimeType}
     decayRate::Float64
-    lastTime::DateTime
+    lastTime::T
     lastValue::Float64
     value::Float64
 end
-EMAFeature(decayRate::Real) = EMAFeature(decayRate, DateTime(), 0.0, 0.0)
-function update!(feature::EMAFeature, time::DateTime, value::Float64)
+EMAFeature{T<:TimeType}(decayRate::Float64, lastTime::T) = EMAFeature(decayRate, lastTime, 0.0, 0.0)
+function update!{T<:TimeType}(feature::EMAFeature{T}, time::T, value::Float64)
     @assert time >= feature.lastTime
     if time == feature.lastTime
         return feature.value
@@ -26,18 +28,18 @@ function update!(feature::EMAFeature, time::DateTime, value::Float64)
 
     feature.value
 end
-function valueat(feature::EMAFeature, time::DateTime)
+function valueat{T<:TimeType}(feature::EMAFeature{T}, time::T)
     @assert time >= feature.lastTime
     feature.value
 end
 
-type EMVFeature
-    ema::EMAFeature
-    ema2::EMAFeature
+type EMVFeature{T<:TimeType}
+    ema::EMAFeature{T}
+    ema2::EMAFeature{T}
     value::Float64
 end
-EMVFeature(decayRate::Real) = EMVFeature(EMAFeature(decayRate), EMAFeature(decayRate), 0.0)
-function update!(feature::EMVFeature, time::DateTime, value::Float64)
+EMVFeature{T<:TimeType}(decayRate::Float64, lastTime::T) = EMVFeature(EMAFeature(decayRate, lastTime), EMAFeature(decayRate, lastTime), 0.0)
+function update!{T<:TimeType}(feature::EMVFeature{T}, time::T, value::Float64)
     @assert time >= feature.ema.lastTime
     if time == feature.ema.lastTime
         return feature.value
@@ -48,7 +50,7 @@ function update!(feature::EMVFeature, time::DateTime, value::Float64)
 
     feature.value = feature.ema2.value - feature.ema.value^2
 end
-function valueat(feature::EMVFeature, time::DateTime)
+function valueat{T<:TimeType}(feature::EMVFeature{T}, time::T)
     @assert time >= feature.ema.lastTime
     feature.value
 end
@@ -56,44 +58,43 @@ end
 # simply keep track of how far we are from the reference time point
 # note that we collapse all negative values to 0.0 since otherwise they
 # would provide information about the future.
-type TimeSinceFeature
-    referenceTime::DateTime
+type TimeSinceFeature{T<:TimeType}
+    referenceTime::T
 end
-TimeSinceFeature() = TimeSinceFeature(DateTime(0,1,1,0,0,0))
-function update!(feature::TimeSinceFeature, time::DateTime)
+function update!{T<:TimeType}(feature::TimeSinceFeature{T}, time::T)
     feature.referenceTime = time
 end
-function update!(feature::TimeSinceFeature, time::DateTime, ignored::Float64)
+function update!{T<:TimeType}(feature::TimeSinceFeature{T}, time::T, ignored::Float64)
     update!(feature, time)
 end
-function valueat(feature::TimeSinceFeature, time::DateTime)
+function valueat{T<:TimeType}(feature::TimeSinceFeature{T}, time::T)
     numMin = Float64(time - feature.referenceTime)/60000 # convert from milliseconds to minutes
     numMin < 0.0 ? 0.0 : numMin
 end
 
 # simply keep track of the last value seen
-type LastValueFeature
-    time::DateTime
+type LastValueFeature{T<:TimeType}
+    time::T
     value::Float64
 end
-LastValueFeature() = LastValueFeature(DateTime(0,1,1,0,0,0), 0.0)
-function update!(feature::LastValueFeature, time::DateTime, value::Float64)
+LastValueFeature{T<:TimeType}(time::T) = LastValueFeature(time, 0.0)
+function update!{T<:TimeType}(feature::LastValueFeature{T}, time::T, value::Float64)
     feature.time = time
     feature.value = value
 end
-function valueat(feature::LastValueFeature, time::DateTime)
+function valueat{T<:TimeType}(feature::LastValueFeature{T}, time::T)
     time < feature.time ? 0.0 : feature.value
 end
 
 
 # Sum all the values and let them decay exponentially
-type DecayFeature
+type DecayFeature{T<:TimeType}
     decayRate::Float64
-    lastTime::DateTime
+    lastTime::T
     value::Float64
 end
-DecayFeature(halfLife::Real) = DecayFeature(exp(log(0.5)/halfLife), DateTime(), 0.0)
-function update!(feature::DecayFeature, time::DateTime, value::Float64)
+DecayFeature{T<:TimeType}(halfLife::Float64, lastTime::T) = DecayFeature(exp(log(0.5)/halfLife), lastTime, 0.0)
+function update!{T<:TimeType}(feature::DecayFeature{T}, time::T, value::Float64)
     @assert time >= feature.lastTime
 
     dt = Float64(time - feature.lastTime)/60000 # convert from milliseconds to minutes
@@ -101,31 +102,31 @@ function update!(feature::DecayFeature, time::DateTime, value::Float64)
     feature.value *= feature.decayRate^dt
     feature.value += value
 end
-function valueat(feature::DecayFeature, time::DateTime)
+function valueat{T<:TimeType}(feature::DecayFeature{T}, time::T)
     @assert time >= feature.lastTime
     dt = Float64(time - feature.lastTime)/60000 # convert from milliseconds to minutes
     feature.value * feature.decayRate^dt
 end
 
 "Sum all the values in a window, the starting point is excluded and the end point included."
-type LagSumFeature
+type LagSumFeature{T<:TimeType}
     lagStart::Dates.TimePeriod # exclusive
     lagEnd::Dates.TimePeriod # inclusive
-    bufferTimes::Array{DateTime,1}
+    bufferTimes::Array{T,1}
     bufferValues::Array{Float64,1}
-    windowTimes::Array{DateTime,1}
+    windowTimes::Array{T,1}
     windowValues::Array{Float64,1}
-    lastTime::DateTime
+    lastTime::T
     value::Float64
 end
-LagSumFeature(lagStart::Dates.TimePeriod, lagEnd::Dates.TimePeriod) = LagSumFeature(lagStart, lagEnd, DateTime[], DateTime[], Float64[], Float64[], DateTime(), 0.0)
-function update!(feature::LagSumFeature, time::DateTime, value::Float64)
+LagSumFeature{T<:TimeType}(lagStart::Dates.TimePeriod, lagEnd::Dates.TimePeriod, lastTime::T) = LagSumFeature(lagStart, lagEnd, T[], Float64[], T[], Float64[], lastTime, 0.0)
+function update!{T<:TimeType}(feature::LagSumFeature{T}, time::T, value::Float64)
     @assert time >= feature.lastTime
     feature.lastTime = time
     unshift!(feature.bufferTimes, time)
     unshift!(feature.bufferValues, value)
 end
-function valueat(feature::LagSumFeature, time::DateTime)
+function valueat{T<:TimeType}(feature::LagSumFeature{T}, time::T)
     @assert time >= feature.lastTime
 
     # move data from the buffer to the window
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,24 @@
 using StreamingTimeSeries
 using Base.Test
+using TimeZones
 
 # EMAFeature
-f = EMAFeature(1)
+f = EMAFeature(1.0, DateTime())
 @test valueat(f, DateTime(2015,8,7)) == 0.0
 update!(f, DateTime(2014,12,1,1,1,1), 1.0)
 @test abs(valueat(f, DateTime(2015,8,7)) - 1.0) < 0.00001
 update!(f, DateTime(2014,12,1,1,1,1), 1.0)
 @test abs(valueat(f, DateTime(2015,8,7)) - 1.0) < 0.00001
 
+f = EMAFeature(1.0, ZonedDateTime(DateTime(), TimeZone("UTC")))
+@test valueat(f, ZonedDateTime(DateTime(2015,8,7), TimeZone("UTC"))) == 0.0
+update!(f, ZonedDateTime(DateTime(2014,12,1,1,1,1), TimeZone("UTC")), 1.0)
+@test abs(valueat(f, ZonedDateTime(DateTime(2015,8,7), TimeZone("UTC"))) - 1.0) < 0.00001
+update!(f, ZonedDateTime(DateTime(2014,12,1,1,1,1), TimeZone("UTC")), 1.0)
+@test abs(valueat(f, ZonedDateTime(DateTime(2015,8,7), TimeZone("UTC"))) - 1.0) < 0.00001
+
 # EMVFeature
-f = EMVFeature(2)
+f = EMVFeature(2.0, DateTime())
 @test valueat(f, DateTime(2015,8,7)) == 0.0
 update!(f, DateTime(2015,8,15,9,0,0), 1.0)
 update!(f, DateTime(2015,8,15,10,0,0), 10.0)
@@ -33,7 +41,7 @@ update!(f, DateTime(2015,8,7,2,1,1))
 @test valueat(f, DateTime(2015,8,7,2,1,1)) == 0.0
 @test valueat(f, DateTime(2015,8,7,2,3,1)) == 2.0
 @test valueat(f, DateTime(2014,8,7,2,3,1)) == 0.0
-f = TimeSinceFeature()
+f = TimeSinceFeature(DateTime())
 update!(f, DateTime(2015,8,7,2,1,1))
 @test valueat(f, DateTime(2015,8,7,2,1,1)) == 0.0
 @test valueat(f, DateTime(2015,8,7,2,3,1)) == 2.0
@@ -43,11 +51,11 @@ f = LastValueFeature(DateTime(2015,8,7), 1.4)
 @test valueat(f, DateTime(2015,8,6)) == 0
 @test valueat(f, DateTime(2015,8,7)) == 1.4
 @test valueat(f, DateTime(2015,8,8)) == 1.4
-f = LastValueFeature()
+f = LastValueFeature(DateTime())
 @test valueat(f, DateTime(2015,8,6)) == 0
 
 # DecayFeature
-f = DecayFeature(1)
+f = DecayFeature(1.0, DateTime())
 @test valueat(f, DateTime(2015,8,7)) == 0.0
 update!(f, DateTime(2014,12,1,1,1,1), 1.0)
 @test valueat(f, DateTime(2015,8,7)) < 0.00001
@@ -55,7 +63,7 @@ update!(f, DateTime(2014,12,1,1,1,1), 1.0)
 @test abs(valueat(f, DateTime(2014,12,1,1,2,1)) - 1) < 0.00001
 
 # LagSumFeature
-f = LagSumFeature(Dates.Minute(1), Dates.Minute(0))
+f = LagSumFeature(Dates.Minute(1), Dates.Minute(0), DateTime())
 @test valueat(f, DateTime(2015,8,7)) == 0.0
 update!(f, DateTime(2015,9,1,1,1,1), 1.0)
 @test valueat(f, DateTime(2015,9,1,1,1,1)) == 1.0
@@ -63,7 +71,7 @@ update!(f, DateTime(2015,9,1,1,2,1), 1.0)
 @test valueat(f, DateTime(2015,9,1,1,2,1)) == 1.0
 @test valueat(f, DateTime(2015,9,1,1,3,1)) == 0.0
 
-f = LagSumFeature(Dates.Minute(6), Dates.Minute(4))
+f = LagSumFeature(Dates.Minute(6), Dates.Minute(4), DateTime())
 update!(f, DateTime(2015,9,1,1,1,1), 1.0)
 update!(f, DateTime(2015,9,1,1,2,1), 1.0)
 @test valueat(f, DateTime(2015,9,1,1,3,1)) == 0.0
